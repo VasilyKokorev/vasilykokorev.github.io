a797b33af604a9c1090c27e214b1221acfb49e4a
VasilyKokorev <45940362+VasilyKokorev@users.noreply.github.com>
2026-02-27 06:18:59 +0000

    Deploying to gh-pages from @ VasilyKokorev/vasilykokorev.github.io@006410f897f7daf4700cffbe9e9f1bfa54b8bd98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,7 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "projects-venus",
           title: 'VENUS',
-          description: "Vast Exploration for Nascent Unexplored Sources",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-lrds",
